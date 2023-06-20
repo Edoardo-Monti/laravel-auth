fd66aa62cfa6d9c7e25ba0b9a4a824edee76dbb5
Your Namede <edoardo.monti.bo@gmail.com>
2023-06-20 15:27:38 +0200

    .

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,9 +3,86 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Model\Admin\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('$posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\rs  $rs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(rs $rs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\rs  $rs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(rs $rs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\rs  $rs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, rs $rs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\rs  $rs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(rs $rs)
+    {
+        //
+    }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Admin\Post;
+
+//helper laravel
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
@@ -12,8 +17,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 20; $i++){
+            $new_post = new Post();
+            $new_post->title = $faker->sentence(3);
+            $new_post->description = $faker->text();
+            $new_post->slug = Str::slug($new_post->title, '-');
+            $new_post->save();
+
+        }
     }
 }
